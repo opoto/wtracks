0f0d6a8f2dcc5945b51d8c98558a211c0fe01613
opoto <olivier.potonniee@gmail.com>
2019-12-22 17:13:52 +0100

    remove ES6 dependency

--- js/utils.js
+++ js/utils.js
@@ -343,7 +343,7 @@ function arrayForEach(array, func) {
 
 if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
+  window.addEventListener('load', function() {
     navigator.serviceWorker.register('./service-worker.js');
   });
 }
