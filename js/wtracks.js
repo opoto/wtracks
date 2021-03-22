c50e3bdcfa4606e40bc7976323fc08fba3022c21
opoto <olivier.potonniee@gmail.com>
2021-03-22 23:48:27 +0100

    remove error log when not an error

--- js/wtracks.js
+++ js/wtracks.js
@@ -2038,18 +2038,21 @@ $(function(){
   var baseLayer = getVal("wt.baseLayer", config.display.map);
   var initialLayer = baseLayers[baseLayer] || baseLayers[config.display.map];
   if (!initialLayer) {
-    var availableLayerNames = "";
+    //var availableLayerNames = "";
     objectForEach(baseLayers, function(name) {
-      availableLayerNames += name + "; ";
+      //availableLayerNames += name + "; ";
       if (!initialLayer) {
         initialLayer = baseLayers[name]; // use first one
+        return true;
       }
     });
+    /* this is not an error: selected and default map have been hidden/deleted
     onerror("no initial layer", {
       "stored": baseLayer,
       "config": config.display.map,
       "available": availableLayerNames
     });
+    */
   }
   if (initialLayer) {
     map.addLayer(initialLayer);
