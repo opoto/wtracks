c71163f3f3c2d2f562605dcd7d714cbf0d29a9f7
opoto <olivier.potonniee@gmail.com>
2021-12-04 19:32:39 +0100

    upgrade workbox

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 if (workbox) {
 
