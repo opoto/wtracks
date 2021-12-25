7ef120cbaa787424bf1c5cccb3d5dfab2201b5b0
EdwardO <edoubrayrie@gmail.com>
2021-12-25 23:49:04 +0100

    Fix overlays restore (regression from #27)

--- js/utils.js
+++ js/utils.js
@@ -79,7 +79,7 @@ function getParameterByName(name, defaultValue) {
   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
-  return results === null ? (isUnset(defaultValue) ? defaultValue : "") : decodeURIComponent(results[1].replace(/\+/g, " "));
+  return results === null ? defaultValue : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 function jsonClone(obj) {
